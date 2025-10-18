a0a0a5c65c667a87706196f10ccb5167ddbab026
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-10-17 22:20:04 -0700

    update party locales[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D[D[D[D[D[Dfishing updating party locales

--- push.sh
+++ push.sh
@@ -1,3 +1,12 @@
 git add .
-git commit -m "feat: Automated Locale Updates"
+
+# Prompt for commit message
+read -p "Enter commit message (or press Enter for default): " commit_msg
+
+# Use default message if none provided
+if [ -z "$commit_msg" ]; then
+    commit_msg="chore: Automated Locale Update"
+fi
+
+git commit -m "$commit_msg"
 git push origin master
